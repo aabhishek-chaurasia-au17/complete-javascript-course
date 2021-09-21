e80872f06518cb6868db1914661b060d533273ed
Abhishek <abhishekc838@gmail.com>
2021-09-21 16:21:08 +0530

    DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+
+// console.log(document.querySelector('.message').textContent);
+
+//
+
+// document.querySelector('.guess').value = 50;
+
+const number = Math.trunc(Math.random() *20) +1
+document.querySelector('.number').textContent = number
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+  if(!guess){
+       document.querySelector('.message').textContent = "No Number!"
+  }
+})
